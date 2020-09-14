b4aaa9cb825e84963b438628e54a727e520c7ee6
harsh1206 <harshprakash1206@gmail.com>
2020-09-14 21:03:58 +0530

    add sign in component

---
+++ src/components/SignIn.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+
+class SignIn extends Component {
+  
+    render(){
+        
+       
+          
+            return(
+             <div>
+                 SignIn
+             </div>
+
+          );
+
+        }    
+
+    }
+
+
+
+
+export default SignIn;
